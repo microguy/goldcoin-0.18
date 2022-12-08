4268acbe78c9fd4046293c2911ca07bc362f0068
barrystyle <barrystyle@westnet.com.au>
2022-12-09 01:36:54 +0800

    consensus: port over the legacy fork activation heights and declarations

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -90,6 +90,16 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = std::numeric_limits<uint64_t>::max();
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = std::numeric_limits<uint64_t>::max();
 
+        // Legacy fork heights
+        consensus.useNewDifficultyProtocol = true;
+        consensus.julyFork = 45000;
+        consensus.octoberFork = 100000;
+        consensus.novemberFork = 103000;
+        consensus.novemberFork2 = 118800;
+        consensus.mayFork = 248000;
+        consensus.julyFork2 = 251230;
+        consensus.febFork = 372000;
+
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
 
@@ -231,6 +241,16 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = std::numeric_limits<uint64_t>::max();
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = std::numeric_limits<uint64_t>::max();
 
+        // Legacy fork heights
+        consensus.useNewDifficultyProtocol = true;
+        consensus.julyFork = 2016;
+        consensus.octoberFork = 2016;
+        consensus.novemberFork = 2016;
+        consensus.novemberFork2 = 2016;
+        consensus.mayFork = 2016;
+        consensus.julyFork2 = 2016;
+        consensus.febFork = 2016;
+
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
 
@@ -319,6 +339,16 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
+
+        // Legacy fork heights
+        consensus.useNewDifficultyProtocol = false;
+        consensus.julyFork = 100000000;
+        consensus.octoberFork = 100000000;
+        consensus.novemberFork = 100000000;
+        consensus.novemberFork2 = 100000000;
+        consensus.mayFork = 100000000;
+        consensus.julyFork2 = 100000000;
+        consensus.febFork = 100000001;
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x00");
