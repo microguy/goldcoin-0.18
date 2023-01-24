b98fc8724f2979531673800e70e846c0b1462bfb
barrystyle <barrystyle@westnet.com.au>
2023-01-24 15:56:31 +0800

    consensus: permanently disable segwit

--- src/validation.cpp
+++ src/validation.cpp
@@ -3167,8 +3167,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 
 bool IsWitnessEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params)
 {
-    LOCK(cs_main);
-    return (VersionBitsState(pindexPrev, params, Consensus::DEPLOYMENT_SEGWIT, versionbitscache) == ThresholdState::ACTIVE);
+    return false;
 }
 
 bool IsNullDummyEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params)
